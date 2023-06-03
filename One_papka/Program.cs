a11b171c8d39b08c31958c7fa02bfa23b6d2b19d
Yorsh <yorshrus34@yandex.ru>
2023-06-03 05:29:52 +0500

    кек

--- One_papka/Program.cs
+++ One_papka/Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine());
-Console.WriteLine(number);
+int number = int.Parse(Console.ReadLine()!);
+int sqr = number * number;
+Console.WriteLine("квадрат числа = " + sqr);
