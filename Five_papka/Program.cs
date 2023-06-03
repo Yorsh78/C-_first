8b2cb5b4c933e170dd5248103259617615fab4ee
Yorsh <yorshrus34@yandex.ru>
2023-06-03 07:30:56 +0500

    на отличненько

---
+++ Five_papka/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine());
+if (number>=100&&number<1000)
+{
+    Console.Write("Последняя цифра числа - " + number%10);
+}
