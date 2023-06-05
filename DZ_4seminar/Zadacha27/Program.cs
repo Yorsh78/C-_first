ddaa3e9d3ab4d06e4385a2dce357557bf1236533
Yorsh <yorshrus34@yandex.ru>
2023-06-06 04:51:14 +0500

    добавил дз4

--- DZ_4seminar/Zadacha27/Program.cs
+++ DZ_4seminar/Zadacha27/Program.cs
@@ -2,3 +2,14 @@
 //452 -> 11
 //82 -> 10
 //9012 -> 12
+
+Console.Write("Ну-ка напиши число, чувак - ");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("Красавчик! Cумма всех цифр твоего опупенного числа - " + sum);
--- DZ_4seminar/Zadacha29/Program.cs
+++ DZ_4seminar/Zadacha29/Program.cs
@@ -1,3 +1,16 @@
 ﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 //1, 2, 5, 7, 19, 4, 76, 3 -> [1, 2, 5, 7, 19,4,76,3]
 //6, 1, 33, 43, 12, 3 ,2, 9 -> [6, 1, 33,43,12,3,2,9]
+
+int [] numbers = new int[8];
+Console.Write("[");
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 11);
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.WriteLine("]");
+int Method (int a)
+{
+    return numbers[a];
+}
