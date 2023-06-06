3df3750ab14905b271741f19b4eacde54b501072
Yorsh <yorshrus34@yandex.ru>
2023-06-06 05:12:13 +0500

    добавил дз3

--- DZ_3seminar/Zadacha19/Program.cs
+++ DZ_3seminar/Zadacha19/Program.cs
@@ -3,3 +3,20 @@
 //14212 -> нет
 //12821 -> да
 //23432 -> да
+
+int num,r,sum=0,t;
+Console.Write("Введите пятизначное число - ");
+num = Convert.ToInt32(Console.ReadLine());
+if(num>9999 && num<100000)
+{
+for(t=num;num!=0;num=num/10)
+{
+r=num % 10;
+sum=sum*10+r;
+}
+if(t==sum)
+Console.WriteLine($"Поздравляю, число {t} является палиндромом.");
+else
+Console.WriteLine($"Соболезную, число {t} не является палиндромом.");
+} else
+Console.WriteLine("Что непонятно? Сказано же, пятизначное число! ");
